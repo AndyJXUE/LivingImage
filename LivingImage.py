d3c576c009b329f19829acba22b7e99642debf5f
AndyXUE <andyxue957@gmail.com>
2023-12-18 15:12:32 +0800

    feat: Output result table and substructures with spectrum colormap

--- LivingImage.py
+++ LivingImage.py
@@ -1,81 +1,34 @@
 import numpy as np
-from functions import ProcessHierachy, save_result, write_centroids, write_subs
+from functions import process_recursively, save_result, save_subs
 from PIL import Image
 
 data_name = "1"  # THE OUPUT FOLDER NAME OF YOUR IMAGE
-image_path = "3.jpg"  # THE INPUT IMAGE PATH
+image_path = r"Sample\7-1.png"  # THE INPUT IMAGE PATH
 
-output_csv_path = "table/%s.csv" % (data_name)
-output_hie_path = "hie/%s.png" % (data_name)
-output_points_path = "points/%s.shp" % (data_name)
+output_csv_path = "Results/%s.csv" % (data_name)
+output_hie_path = "Subs/%s.png" % (data_name)
 
 print("--------------------- Processing ---------------------")
-focus = "light"  # "light" or "dark"
-
+focus = "dark"  # "light" or "dark"
 
 inputraster = np.array(Image.open(image_path)).astype(np.int64)
 if len(inputraster.shape) > 2:
     inputraster = np.dot(inputraster[..., 0:3], [0.2989, 0.1140, 0.5870])
 
-(
-    return_list,
-    s_list,
-    d_list,
-    lr_list,
-    area_list,
-    centers_list,
-    output,
-    d_index_list,
-    xy_list,
-) = ([], [], [], [], [], [], [], [], [])
-xy = np.zeros(2, dtype=int)
-shape = np.shape(inputraster)
+results = process_recursively(inputraster, focus)
 
+i = len(results)
+d_array = np.array([item['d'] for item in results])
+s_array = np.array([item['s'] for item in results])
+lr_array = np.array([item['lr'] for item in results])
+output_list = [item['output'] for item in results]
+xy_list = [item['xy'] for item in results]
 
-i, d, decs = 1, 1, 0
-
-
-while d != 0:
-    if i == 1:
-        (
-            return_list,
-            _,
-            s,
-            lr,
-            centroid,
-            xy,
-            d_index,
-            lb_images,
-            areas,
-        ) = ProcessHierachy([inputraster], xy, focus)
-    else:
-        return_list, d, s, lr, centroid, xy, d_index, _, areas = ProcessHierachy(
-            return_list, xy, focus
-        )
-
-    if len(return_list) != 0:
-        print("--------------------- hierachy %d has done ---------------------" % (i))
-        print("Hierachy %d, D=%d, S=%d, lr=%d" % (i, d, s, lr))
-
-        i += 1
-        s_list.append(s)
-        lr_list.append(lr)
-        d_list.append(d)
-        xy_list.append(xy)
-
-    centers_list.append(np.array(centroid))
-    d_index_list.append(d_index)
-    output.append(return_list)
-    area_list.append(areas)
-
-    if len(return_list) == 0 and i == 1:
-        break
-
-decs = np.array(d_list).sum()
+decs = np.array(d_array).sum()
 v = i * decs
-print("Final result: lr = %d, V = %d" % (np.array(lr_list).sum(), v))
+lr = lr_array.sum()
+print("--------------------- Finished ---------------------")
+print("Final result: lr = %d, V = %d" % (lr, v))
 
-
-# save_result(output_csv_path, d_list, s_list, lr_list, i)
-# write_centroids(output_points_path, centers_list, d_index_list, area_list)
-# write_subs(output_hie_path, output, inputraster, lb_images, xy_list)
+save_result(output_csv_path, d_array, s_array, lr_array, i)
+save_subs(output_hie_path, output_list, inputraster, xy_list)
--- functions.py
+++ functions.py
@@ -2,11 +2,10 @@ import cv2
 import os
 import numpy as np
 import pandas as pd
-from tqdm import tqdm
-from osgeo import ogr
-from scipy.stats import gaussian_kde
-from scipy.ndimage import gaussian_filter
+import matplotlib.pyplot as plt
 
+from tqdm import tqdm
+from scipy.stats import gaussian_kde
 
 def percent_clip(img_array, percent):
     # 将图像转换为numpy数组
@@ -24,17 +23,10 @@ def percent_clip(img_array, percent):
 
     return img_array
 
-
 def BinarizeRaster(inputraster, focus="dark"):
     array = np.ravel(inputraster)
-    # 找到包含值为255的元素的索引
 
-    if focus == "light":
-        idx = np.where(array == -1)
-    else:
-        idx = np.where(array == -1)
-
-    # 删除包含值为255的元素
+    idx = np.where(array == -1)
     array = np.delete(array, idx)
     if len(array):
         in_raster_mean = np.mean(array)
@@ -56,16 +48,11 @@ def BinarizeRaster(inputraster, focus="dark"):
     del array, idx
     return reclass
 
-
 def head_tail_breaks(array, break_per=0.4):
     rats = []
     rat_in_head, head_mean, ht_index = 0, 0, 1
 
     array = np.ravel(array)
-
-    # idx = np.where(array < 0)
-
-    # array = np.delete(array, idx)
 
     while (rat_in_head <= break_per) and (len(array) > 1) and np.mean(array) > 0:
         mean = np.mean(array)
@@ -90,7 +77,6 @@ def head_tail_breaks(array, break_per=0.4):
     del head_mean, array, rats
     return ht_index
 
-
 def Clip(img, lb_images, stats, centers, xy_last, thre=0.00001, focus="dark"):
     # 获取所有连通区域的面积
     areas = stats[:, cv2.CC_STAT_AREA]
@@ -110,10 +96,7 @@ def Clip(img, lb_images, stats, centers, xy_last, thre=0.00001, focus="dark"):
         region = img[y : y + h, x : x + w]
         region_lb = lb_images[y : y + h, x : x + w]
 
-        if focus == "light":
-            region = np.where(region_lb == (i + 1), region, -1)
-        elif focus == "dark":
-            region = np.where(region_lb == (i + 1), region, -1)
+        region = np.where(region_lb == (i + 1), region, -1)
 
         xy = np.array([x, y])
         centroid = centers[i].astype(int)
@@ -127,32 +110,28 @@ def Clip(img, lb_images, stats, centers, xy_last, thre=0.00001, focus="dark"):
     del img, lb_images, stats, centers
     return regions_list, centers_list, original_xy
 
-
-def ProcessHierachy(regions_list, xy_last_list, focus, break_per=0.4, first=False):
+def process_hierarchy(regions_list, xy_last_list, focus, break_per=0.4, first=False):
     return_list, centers_list, xy_list, d_index, areas_list = [], [], [], [], []
     s, d, lr = 0, 0, 0
 
     with tqdm(total=len(regions_list), desc="Processing regions") as pbar:
-        for i in range(0, len(regions_list)):
-            region = regions_list[i]
+        for i, region in enumerate(regions_list):
             bin_image = BinarizeRaster(region, focus)
 
             num_labels, lb_images, stats, centers = cv2.connectedComponentsWithStats(
                 bin_image, connectivity=4
             )
-            stats_ori = stats[:, cv2.CC_STAT_AREA]
             num_labels -= 1
-            stats = stats[1:, :]
-            centers = centers[1:, :]
+            stats = stats[1:]
+            centers = centers[1:]
 
-            areas = stats[:, cv2.CC_STAT_AREA]  # 去除背景
-            areas = np.sqrt(areas)
+            areas = np.sqrt(stats[:, cv2.CC_STAT_AREA])  # Remove background
             ht_index = head_tail_breaks(areas, break_per)
 
-            if ht_index > 2 or first == True:
+            if ht_index > 2 or first:
                 d_index.append(i)
                 regions_list_new, centers, xy = Clip(
-                    region, lb_images, stats, centers, xy_last_list[i], focus=focus
+                    region, lb_images, stats, centers, xy_last_list[i], focus=focus 
                 )
                 return_list.extend(regions_list_new)
                 centers_list.extend(centers)
@@ -164,146 +143,63 @@ def ProcessHierachy(regions_list, xy_last_list, focus, break_per=0.4, first=Fals
                 areas_list.extend(areas)
             pbar.update(1)
 
-    del regions_list, xy_last_list, num_labels, stats, centers, ht_index
-    return return_list, d, s, lr, centers_list, xy_list, d_index, lb_images, areas_list
+    return return_list, d, s, lr, xy_list, d_index,
 
+def process_recursively(inputraster, focus):
+    i = 0
+    xy = np.zeros(2, dtype=int)
+    results = []
 
-def save_result(csv_path, d_list, s_list, lr_list, i):
-    # 创建一个空的 DataFrame，只包含列名
-    df = pd.DataFrame(columns=["I", "D", "S", "LR", "H"])
-    d = np.array(d_list)
-    s = np.array(s_list)
-    lr = np.array(lr_list)
-    h = lr / s
-    arr = np.arange(1, i)
-    lr = np.append(lr, np.sum(lr_list))
-    s = np.append(s, np.sum(s_list))
-    arr = np.append(arr, 0)
-    d = np.append(d, d.sum())
+    while True:
+        return_list, d, s, lr, xy, d_index = process_hierarchy(
+            [inputraster] if i == 0 else return_list, xy, focus)
 
-    # 将这些数组转换为 pandas Series
-    s1 = pd.Series(arr, name="I")
-    s2 = pd.Series(d, name="D")
-    s3 = pd.Series(s, name="S")
-    s6 = pd.Series(lr, name="LR")
-    s7 = pd.Series(h, name="H")
+        if len(return_list) == 0:
+            break
 
-    # 将这三列 Series 合并成一个 DataFrame
-    df = pd.concat([s1, s2, s3, s6, s7], axis=1)
+        print(f"--------------------- hierarchy {i+1} has done ---------------------")
+        print(f"Hierarchy {i}, D={d}, S={s}, lr={lr}")
 
-    # 将 I 字段格式化为整数
-    df["I"] = df["I"].astype(int)
+        i += 1
+        results.append({
+            'output': return_list,
+            's': s,
+            'd': d,
+            'lr': lr,
+            'd_index': d_index,
+            'xy': xy
+        })
 
-    # 将 H 字段格式化为小数点后两位
-    formatted_h = df["H"].apply(lambda x: "{:.2f}".format(x))
-    df["H"] = formatted_h
+    return results
 
-    # 将 DataFrame 写入 CSV 文件
+def save_result(csv_path, d_array, s_array, lr_array, i):
+
+    folder, _ = os.path.split(csv_path)
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    h_array = lr_array / s_array
+    i_array = np.arange(1, i+1).astype(int)
+    
+    u_array = np.zeros(i)
+    u_array[:-1] = s_array[:-1] - d_array[1:]
+    u_array[-1] = s_array[-1]
+    
+    per_array = (s_array-u_array) / s_array
+
+    # Create a DataFrame directly from the arrays
+    df = pd.DataFrame({
+        "I": i_array,
+        "D": d_array,
+        "S": s_array,
+        "H": h_array.round(2),  # Round 'H' to two decimal places
+        "U": u_array.astype(int),
+        "%": per_array.round(2),
+        "LR": lr_array
+    })
+
+    # Write the DataFrame to a CSV file
     df.to_csv(csv_path, index=False)
-
-
-# Function to create a directory if it doesn't exist
-def create_directory_if_not_exists(directory_path):
-    if not os.path.exists(directory_path):
-        os.makedirs(directory_path)
-
-
-def write_centroids(cen_path, centers_list, d_index_list, area_list):
-    folder, _ = os.path.split(cen_path)
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-
-    driver = ogr.GetDriverByName("ESRI Shapefile")
-    out_ds = driver.CreateDataSource(cen_path)
-    out_layer = out_ds.CreateLayer(
-        os.path.splitext(cen_path)[0], geom_type=ogr.wkbPoint
-    )
-
-    # 创建字段定义
-    field_def_id = ogr.FieldDefn("id", ogr.OFTInteger)
-    field_def_d = ogr.FieldDefn("d", ogr.OFTInteger)
-    field_def_a = ogr.FieldDefn("a", ogr.OFTInteger)
-
-    # 将字段定义添加到图层
-    out_layer.CreateField(field_def_id)
-    out_layer.CreateField(field_def_d)
-    out_layer.CreateField(field_def_a)
-
-    for hie in range(len(centers_list)):
-        for p in range(len(centers_list[hie])):
-            a = area_list[hie][p] * (hie + 1)
-
-            x_geo = centers_list[hie][p, 0]
-            y_geo = -centers_list[hie][p, 1]
-
-            feature = ogr.Feature(out_layer.GetLayerDefn())
-            feature.SetField("id", hie + 1)
-            feature.SetField("a", np.sqrt(a))
-            if hie < len(centers_list) - 1:
-                if p in d_index_list[hie + 1]:
-                    feature.SetField("d", 1)
-                else:
-                    feature.SetField("d", 0)
-            else:
-                feature.SetField("d", 0)
-
-            wkt = f"POINT ({x_geo} {y_geo})"
-            point = ogr.CreateGeometryFromWkt(wkt)
-            feature.SetGeometry(point)
-            out_layer.CreateFeature(feature)
-    # 释放资源
-    out_ds = None
-
-
-def write_dcentroids(cen_path, centers_list, d_index_list, area_list):
-    folder, _ = os.path.split(cen_path)
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-
-    driver = ogr.GetDriverByName("ESRI Shapefile")
-    out_ds = driver.CreateDataSource(cen_path)
-    out_layer = out_ds.CreateLayer(
-        os.path.splitext(cen_path)[0], geom_type=ogr.wkbPoint
-    )
-
-    # 创建字段定义
-    field_def_id = ogr.FieldDefn("id", ogr.OFTInteger)
-    field_def_d = ogr.FieldDefn("w", ogr.OFTInteger)
-
-    # 将字段定义添加到图层
-    out_layer.CreateField(field_def_id)
-    out_layer.CreateField(field_def_d)
-
-    for hie in range(len(centers_list) - 1):
-        for p in range(len(centers_list[hie])):
-            x_geo = centers_list[hie][p, 0]
-            y_geo = -centers_list[hie][p, 1]
-
-            if hie < len(centers_list) - 1:
-                if p in d_index_list[hie + 1]:
-                    # for t in range((hie+1)**2):
-                    feature = ogr.Feature(out_layer.GetLayerDefn())
-                    a = area_list[hie][p] * (hie + 1)
-                    feature.SetField("id", hie + 1)
-                    feature.SetField("w", a)
-
-                    wkt = f"POINT ({x_geo} {y_geo})"
-                    point = ogr.CreateGeometryFromWkt(wkt)
-                    feature.SetGeometry(point)
-                    out_layer.CreateFeature(feature)
-    # 释放资源
-    out_ds = None
-
-
-def flatten(lst):
-    result = []
-    for elem in lst:
-        if isinstance(elem, list):
-            result.extend(flatten(elem))
-        else:
-            result.append(elem)
-    return result
-
 
 def heatmap(heat_path, fcenters_list, inputraster, width, height):
     folder, _ = os.path.split(heat_path)
@@ -339,7 +235,6 @@ def heatmap(heat_path, fcenters_list, inputraster, width, height):
     add_img = cv2.addWeighted(inputraster, 0.3, heat_img, 0.7, 0)
     cv2.imwrite(heat_path, add_img)
 
-
 def write_decs(output_hie_path, output, inputraster, lb_images, xy_list, d_index_list):
     # All substructures, except H1
     folder, _ = os.path.split(output_hie_path)
@@ -365,37 +260,32 @@ def write_decs(output_hie_path, output, inputraster, lb_images, xy_list, d_index
             base = base + himg
     cv2.imwrite(output_hie_path, base)
 
+def apply_colormap(input_image, colormap_name):
+    cmap = plt.get_cmap(colormap_name)
+    colored_image = (cmap(input_image) * 255).astype(np.uint8)
+    return colored_image
 
-def write_subs(output_hie_path, output, inputraster, lb_images, xy_list):
+def save_subs(output_hie_path, output, inputraster, xy_list):
     folder, _ = os.path.split(output_hie_path)
     if not os.path.exists(folder):
         os.makedirs(folder)
-    himgs = []
-    for i in range(0, len(output)):
-        himg = np.zeros(np.shape(inputraster)).astype(np.int8)
 
-        if i == 0:
-            himg = np.where(lb_images > 0, 1, 0)
-        else:
-            lr = output[i]
-            # with tqdm(total=len(lr), desc='Processing sub') as pbar:
-            for j in range(len(lr)):
-                # 获取当前影像和坐标位置
-                current_image = np.where(lr[j] > 0, i + 1, 0).astype(np.int8)
-                y, x = xy_list[i][j]
-                # 将当前影像插入到新影像中
-                himg[
-                    x : x + current_image.shape[0], y : y + current_image.shape[1]
-                ] = current_image
+    himgs = []
+    for i, lr in enumerate(output):
+        himg = np.zeros_like(inputraster, dtype=np.int8)
+
+        for j, current_image in enumerate(lr):
+            y, x = xy_list[i][j]
+            current_image = np.where(current_image > 0, i + 1, 0).astype(np.int8)
+            x_end, y_end = x + current_image.shape[0], y + current_image.shape[1]
+            himg[x:x_end, y:y_end] = current_image
+
         himgs.append(himg)
 
-    base = np.zeros(np.shape(inputraster)).astype(np.int8)
-    for i in range(len(himgs)):
-        h = himgs[i]
-        if i == 0:
-            base += h
-        else:
-            base[h != 0] = 0
-            base = base + h
+    base = np.zeros_like(inputraster, dtype=np.int8)
+    for h in himgs:
+        base = np.where(h != 0, h, base)
+    base = base/base.max()
 
-    cv2.imwrite(output_hie_path, base)
+    colored_image = apply_colormap(base, "Spectral")
+    cv2.imwrite(output_hie_path, colored_image)
